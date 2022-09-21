643f83c3cf98b087e66b1188d25b5e4901894e11
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2022-09-21 21:26:54 +0530

    css added

--- script.js
+++ script.js
@@ -7,15 +7,22 @@ let cashTable = document.querySelector('.cash-output');
 
 let notesAvailable = [200,500,100,20,10,5,1];
 
+function validateAmount()
+{
+
+}
 
 
-
-function  calcChangeAmount(returnMOney)
+function  calculateChangeAmount(returnMOney)
 {
     for(let i=0; i<notesAvailable.length; i++) 
     {
         let noOfnotes = Math.trunc(returnMOney/availableNotes[i]);
-        returnMOney = returnMOney%availableNotes[i];
+        returnMOney = returnMOney % availableNotes[i];
         noOfnotes[i].innerText = noOfnotes;
     }
 }
+
+
+
+submitBtn.addEventListener
